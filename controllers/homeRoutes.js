6ea225023c2f2aa0800fea773f45a2a1bbb414a8
Mitchell <mitchellm27@gmail.com>
2022-04-02 11:21:03 -0700

    fixed create page render issue.

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -51,7 +51,7 @@ router.get('/profile', withAuth, async (req, res) => {
   try {
     const userData = await User.findByPk(req.session.user_id, {
       attributes: { exclude: ['password'] },
-      include: [{ model: Project }],
+      include: [{ model: Post }],
     });
 
     const user = userData.get({ plain: true });
